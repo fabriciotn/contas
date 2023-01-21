405b97aea5dfb7289579f0aaee437ff82f8ce6b6
Fabricio Nascimento <fabricio@MacBook-Pro-de-Fabricio.local>
2023-01-21 17:59:09 -0300

    Inclusão do projeto spring boot vazio

---
+++ src/test/java/br/com/contas/contas/ContasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.contas.contas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
